af67edeccf9d52577504476962bdc76872e64437
mustafautku <mustafautku@mustafautku>
2022-08-04 23:33:32 +0300

    data load preperation completed.

--- test/rtree/realDataLoad/realDataLoad.sh
+++ test/rtree/realDataLoad/realDataLoad.sh
@@ -2,7 +2,7 @@
 # default shell is already bash
 
 # NOTE:(UK) This file uses real data sets and load it into R*tree and STR-tree.
-# sample usage: ...$./realDataLoad.sh nyc_cb minx miny maxx maxy
+# sample usage: ...$./realDataLoad.sh nyc public nyc_subway_stations  -- $1: database  $2:schema $3:spatial-table
 # my execs:
 bindir=$HOME/eclipse-workspace/test-build/
 
@@ -17,35 +17,35 @@ export bP=100    # buffer size (num of buffers). Used in ext-sort
 mkdir -p  $HOME/eclipse-workspace/test-build/data  # -p flag: mk dir only if dir does not exist.
 datadir=$HOME/eclipse-workspace/test-build/data
 
-#datafilename=$1  #this takes the first arguman from commandline..
-datafile=${datadir}/$1
+datafile=${datadir}/$3
 
 mkdir -p $HOME/eclipse-workspace/test-build/plt   # -p flag: mk dir only if dir does not exist.
 pltdir=$HOME/eclipse-workspace/test-build/plt
 
 mkdir -p $HOME/eclipse-workspace/test-build/database    # -p flag: mk dir only if dir does not exist.
 dbdir=$HOME/eclipse-workspace/test-build/database
-treename=tree_$1
+treename=tree_$3
 
 echo Retrieve data from PGSQL
 
-geomtype=$(PGPASSWORD=mypg1 psql -h localhost -d nyc -U postgres -X -A -F' ' -w -t -c "
-select gc.type from geometry_columns gc where gc.f_table_name = "\'$1\')
+geomtype=$(PGPASSWORD=mypg1 PGOPTIONS=--search_path=$2 psql -h localhost -d $1 -U postgres -X -A -F' ' -w -t -c "
+select gc.type from geometry_columns gc where gc.f_table_name = "\'$3\')
 
 # e/t in "if else..." format is important. [[ x == y ]];  --> valid.    [[x == y ]];  --> invalid.   i.e.
 # For more clarifications, look at My PostGIS scripts/ 05-GenerateMBRdiagonals.sql
 if [[ $geomtype == POINT ]];
 then
-	PGPASSWORD=mypg1 psql -h localhost -d nyc -U postgres -X -A -F' ' -w -t -c "
-	with temp as(SELECT gid, ST_X(geom) as xl, st_y(geom) as yl, ST_X(geom) as xh, st_y(geom) as yh FROM "$1")
+	PGPASSWORD=mypg1 PGOPTIONS=--search_path=$2 psql -h localhost -d $1 -U postgres -X -A -F' ' -w -t -c "
+	with temp as(SELECT gid, ST_X(geom) as xl, st_y(geom) as yl, ST_X(geom) as xh, st_y(geom) as yh FROM "$3")
 	select 1 as INSERT,gid,xl,yl,xh,yh, min(xl) over() as minx,min(yl) over() as miny,max(xh) over() as maxx,max(yh) over() as maxy from temp" > t
-elif [[ $geomtype == MULTIPOLYGON ]] || [[ $geomtype == POLYGON ]];
-then
-	PGPASSWORD=mypg1 psql -h localhost -d nyc -U postgres -X -A -F' ' -w -t -c "
+#elif [[ $geomtype == MULTIPOLYGON ]] || [[ $geomtype == POLYGON ]];
+# handle all other cases similar.
+else
+	PGPASSWORD=mypg1 PGOPTIONS=--search_path=$2 psql -h localhost -d $1 -U postgres -X -A -F' ' -w -t -c "
 	with temp1 as (
 		select ncb.gid as gid, st_envelope(ncb.geom) as geom
-		from "$1" ncb
-		where st_numgeometries(ncb.geom)=1   -- ignore multiple polygons. There are 118 of them.
+		from "$3" ncb
+		where st_numgeometries(ncb.geom)=1   -- ignore multiple polygons. There are 118 census blocks (multipolygon), 0 streets(multipolyline)
 	    ),
 	temp2 as(
 		select 1 as INSERT,temp1.gid,  -- 1 means INSERT in libspatialindex.
@@ -58,8 +58,8 @@ then
 #elif [[ $geomtype == MULTILINESTRING ]] || [[ $geomtype == LINESTRING ]];
 #then
 
-else
-	echo "Geometry not supported.."
+#else
+#	echo "Geometry not supported.."
 fi
 
 awk '{print $1,$2,$3,$4,$5,$6 }' t > $datafile
@@ -77,7 +77,7 @@ echo Load R*-Tree ${treename}_Dyn
 # > r  : redirect std output to file r with replacing r.  >> : redirect and append to file r.
 #time ../../test-rtree-RTreeLoad $datafile $treename $capacity intersection > r 2>&1    # intersection is query type. But we are not sending any query!!
 
-time ${bindir}/test-rtree-RTreeLoad ${datadir}/$1 ${dbdir}/$treename $capacity intersection 2>r 1>${pltdir}/pltDynLevel0     #redirect cerr to 'r' AND redirect cout to plt...file
+time ${bindir}/test-rtree-RTreeLoad ${datadir}/$3 ${dbdir}/$treename $capacity intersection 2>r 1>${pltdir}/pltDynLevel0     #redirect cerr to 'r' AND redirect cout to plt...file
 awk '{if ($1 ~ /Time/  ||
 		  $1 ~ /TOTAL/ ||
 		  $1 ~ /Buffer/ ||
@@ -94,7 +94,7 @@ rm -rf r
 echo -------------
 
 echo Load STR-Tree ${treename}_STR
-time ${bindir}/test-rtree-RTreeBulkLoad ${datadir}/$1 ${dbdir}/$treename $capacity $fillfactor 1 $pS $bP 2> r 1>${pltdir}/pltSTRLevel0;  #redirect cerr to 'r' AND redirect cout to plt...file
+time ${bindir}/test-rtree-RTreeBulkLoad ${datadir}/$3 ${dbdir}/$treename $capacity $fillfactor 1 $pS $bP 2> r 1>${pltdir}/pltSTRLevel0;  #redirect cerr to 'r' AND redirect cout to plt...file
 	awk '{if ($1 ~ /Time/  ||
 		  $1 ~ /TOTAL/ ||
 		  $1 ~ /Buffer/ ||
@@ -112,13 +112,15 @@ time ${bindir}/test-rtree-RTreeBulkLoad ${datadir}/$1 ${dbdir}/$treename $capaci
 	echo -------------
 
 gnuplot -persist <<-EOFMarker
-	set title "Leaf MBRs of R*-tree "
+	set title "Leaf MBRs of R*-tree holding $3"
 	set xlabel "x"
 	set ylabel "y"
 	unset logscale x
 	unset logscale y
 	set xrange[$minx:$maxx]
 	set yrange[$miny:$maxy]
+	unset xtics
+	unset ytics
 	set grid
 	set size square
 	set style line 2 lc rgb 'black' linetype 1 lw 1  # unset style line 2
@@ -127,13 +129,15 @@ EOFMarker
 
 
 gnuplot -persist <<-EOFMarker
-	set title "Leaf MBRs of STR-tree holding $ds objects"
+	set title "Leaf MBRs of STR-tree holding $3"
 	set xlabel "x"
 	set ylabel "y"
 	unset logscale x
 	unset logscale y
 	set xrange[$minx:$maxx]
 	set yrange[$miny:$maxy]
+	unset xtics
+	unset ytics
 	set grid
 	set size square
 	set style line 2 lc rgb 'black' linetype 1 lw 1  # unset style line 2
